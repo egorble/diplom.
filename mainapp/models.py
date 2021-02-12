4f19754a26a34e665dc7d2d8324697d7444ec4d4
egorble <egor4042007@gmail.com>
2021-02-12 16:29:29 +0200

    make it better

--- mainapp/models.py
+++ mainapp/models.py
@@ -128,8 +128,8 @@ class CartProduct(models.Model):
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
-    def __str__(self):
-        return "Продукт: {} (для корзины)".format(self.content_object.title)
+    # def __str__(self):
+    #     return "Продукт: {} (для корзины)".format(self.content_object.title)
 
     def save(self, *args, **kwargs):
         self.final_price = self.qty * self.content_object.price
