ee5534baa07adc9555a9783ae131ff8f7bb4d44d
egorble <egor4042007@gmail.com>
2021-02-13 14:28:17 +0200

    make it better

--- shop/settings.py
+++ shop/settings.py
@@ -11,7 +11,28 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+ADMINS = (
+     ('Yegor', 'egor4042007@gmail.com'),
+)
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
